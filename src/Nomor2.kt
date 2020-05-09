73930c0b749a8f8b83b94f900e8e10dd94534959
Abika Chairul Yusri <35727186+Abikayusri@users.noreply.github.com>
2020-05-09 15:16:49 +0700

    Week 1 Assignment

--- src/Nomor2.kt
+++ src/Nomor2.kt
@@ -1,13 +1,9 @@
-fun main(args: Array<String>) {
+fun main() {
 
 val data = arrayOf(1,2,3,4,5,6,7,8,9,10)
     println("Hasil dari Array")
     println("================")
     for (i in data){
-        if (i%2 == 0){
-            println("Nilai $i adalah genap")
-        } else {
-            println("Nilai $i adalah ganjil")
-        }
+        if (i%2 == 0) println("Nilai $i adalah genap")
     }
 }
